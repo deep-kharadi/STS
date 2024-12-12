4f44c7dd14af2c7802efc7e81b0040f97fb7bc63
Deep Kharadi <deepkrushnakant.kharadi@student.griffith.ir>
2024-12-12 11:46:58 +0000

    Docket configuration file added

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+# Base image
+FROM node:18-alpine
+
+# Set working directory
+WORKDIR /usr/src/app
+
+# Copy package.json and package-lock.json
+COPY package*.json ./
+
+# Install dependencies
+RUN npm install
+
+# Copy the application code
+COPY . .
+
+# Expose the backend port
+EXPOSE 8000
+
+# Start the server
+CMD ["npm", "run", "start"]
--- src/Lib/Utils/Connection.ts
+++ src/Lib/Utils/Connection.ts
@@ -1,7 +1,7 @@
 import mongoose from "mongoose"
 
 export const connectDB = (): void => {
-	const mongoURI: string = process.env.MONGODB_URI ?? "mongodb://127.0.0.1:27017/sts"
+	const mongoURI = "mongodb+srv://deepkharadi9:3ANArrcd9QzvBXl3@cluster0.swl8d.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 	mongoose.connect(mongoURI)
 		.then(() => {
@@ -11,3 +11,22 @@ export const connectDB = (): void => {
 			console.error("MongoDB connection error:", error)
 		})
 }
+
+
+// const mongoose = require('mongoose');
+// const uri = "mongodb+srv://deep:deep@cluster0.swl8d.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+// const clientOptions = { serverApi: { version: '1', strict: true, deprecationErrors: true } };
+
+// async function run() {
+//   try {
+//     // Create a Mongoose client with a MongoClientOptions object to set the Stable API version
+//     await mongoose.connect(uri, clientOptions);
+//     await mongoose.connection.db.admin().command({ ping: 1 });
+//     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+//   } finally {
+//     // Ensures that the client will close when you finish/error
+//     await mongoose.disconnect();
+//   }
+// }
+// run().catch(console.dir);
